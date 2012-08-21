63e8f22976e05558842ad3916bac854e99f965f7
Simon Cropp <simon.cropp@gmail.com>
2012-08-21 20:59:51 +1000

    more

--- Fody/OfMethodHandler.cs
+++ Fody/OfMethodHandler.cs
@@ -6,24 +6,32 @@ public partial class ModuleWeaver
 
     void HandleOfMethod(Instruction instruction, ILProcessor ilProcessor)
     {
-        var previous = instruction.Previous;
-        var ldString = GetLdString(previous);
-        var strings = ldString.Split('.');
-        var typeName = string.Join(".", strings.Take(strings.Length - 1));
-        var methodName = strings.Last();
+        //Info.OfMethod("AssemblyToProcess","MethodClass","InstanceMethod");
 
-        var typeDefinition = GetTypeDefinition(typeName);
+        var methodNameInstruction = instruction.Previous;
+        var methodName = GetLdString(methodNameInstruction);
 
-        var method = typeDefinition.Methods.FirstOrDefault(x => x.Name == methodName);
+        var typeNameInstruction = methodNameInstruction.Previous;
+        var typeName = GetLdString(typeNameInstruction);
 
-        if (method == null)
+        var assemblyNameInstruction = typeNameInstruction.Previous;
+        var assemblyName = GetLdString(assemblyNameInstruction);
+
+        var typeDefinition = GetTypeDefinition(assemblyName, typeName);
+
+        var methodDefinition = typeDefinition.Methods.FirstOrDefault(x => x.Name == methodName);
+        if (methodDefinition == null)
         {
-            throw new WeavingException(string.Format("Could not find method named '{0}'.", ldString));
+            throw new WeavingException(string.Format("Could not find method named '{0}'.", methodName));
         }
 
-        previous.OpCode = OpCodes.Ldtoken;
-        previous.Operand = method;
+        var methodReference = ModuleDefinition.Import(methodDefinition);
+
+        ilProcessor.Remove(typeNameInstruction);
+        ilProcessor.Replace(assemblyNameInstruction, Instruction.Create(OpCodes.Ldtoken, methodReference));
+
         instruction.Operand = getMethodFromHandle;
+        
         ilProcessor.InsertAfter(instruction,Instruction.Create(OpCodes.Castclass,methodInfoType));
     }
 
