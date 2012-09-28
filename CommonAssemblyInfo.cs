dab01700aa026711b47120096726c5642e2d9160
Simon Cropp <simon.cropp@gmail.com>
2012-09-28 22:11:39 +1000

    bump version, make package project level

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("InfoOf")]
 [assembly: AssemblyProduct("InfoOf")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
