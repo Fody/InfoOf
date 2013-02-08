4dd5cb70f6e5fe949bfa308a2a53148198e202ed
Simon Cropp <simon.cropp@gmail.com>
2013-02-08 20:02:01 +1100

    bump refs

--- Tests/AssemblyExtensions.cs
+++ Tests/AssemblyExtensions.cs
@@ -3,11 +3,9 @@ using System.Reflection;
 
 public static class AssemblyExtensions
 {
-
     public static dynamic GetInstance(this Assembly assembly, string className)
     {
         var type = assembly.GetType(className, true);
-        //dynamic instance = FormatterServices.GetUninitializedObject(type);
         return Activator.CreateInstance(type);
     }
 }
