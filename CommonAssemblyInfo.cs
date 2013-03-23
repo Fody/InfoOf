fe6ae977cbaff491c370d4127a18ba641ca6258f
Simon Cropp <simon.cropp@gmail.com>
2013-03-23 22:13:31 +1100

    move to fody nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("InfoOf")]
 [assembly: AssemblyProduct("InfoOf")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
