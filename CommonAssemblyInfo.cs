8be0af253fa25a8bcc60bed6bb06bec0284da286
Simon Cropp <simon.cropp@gmail.com>
2013-12-17 07:42:52 +1100

    make it a dev nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("InfoOf")]
 [assembly: AssemblyProduct("InfoOf")]
-[assembly: AssemblyVersion("0.11.0")]
-[assembly: AssemblyFileVersion("0.11.0")]
+[assembly: AssemblyVersion("0.11.1")]
+[assembly: AssemblyFileVersion("0.11.1")]
