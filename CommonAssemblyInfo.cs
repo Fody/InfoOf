4670aeb7645c1606b19b51483eae1f1a1ac40385
Simon Cropp <simon.cropp@gmail.com>
2017-11-10 08:09:30 +1100

    fix version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("InfoOf")]
 [assembly: AssemblyProduct("InfoOf")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.1.1")]
