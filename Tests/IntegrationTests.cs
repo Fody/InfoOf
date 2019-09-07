f988b7cdba5af70f4a538f7dc31d24a84e80c152
Simon Cropp <simon.cropp@gmail.com>
2019-09-07 15:10:03 +1000

    Xunit.ApprovalTests

--- Tests/IntegrationTests.cs
+++ Tests/IntegrationTests.cs
@@ -6,7 +6,7 @@ using Xunit;
 using Xunit.Abstractions;
 
 public partial class IntegrationTests :
-    XunitLoggingBase
+    XunitApprovalBase
 {
     static Assembly assembly;
     static TestResult testResult;
--- Tests/TypeNameParserTests.cs
+++ Tests/TypeNameParserTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 using Xunit.Abstractions;
 
 public class TypeNameParserTests :
-    XunitLoggingBase
+    XunitApprovalBase
 {
     [Fact]
     public void Simple()
