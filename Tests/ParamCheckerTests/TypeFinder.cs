d970b77a18078682563db13458dcbb5654be0218
Simon Cropp <simon.cropp@gmail.com>
2021-11-17 20:30:34 +1100

    try fix build

--- Tests/ParamCheckerTests/TypeFinder.cs
+++ Tests/ParamCheckerTests/TypeFinder.cs
@@ -5,9 +5,10 @@ public static class TypeFinder
 {
     public static TypeDefinition Find<T>()
     {
-        var location = typeof(T).Assembly.CodeBase;
-        location = location.Replace("file:///", "");
+        var location = typeof(T).Assembly.Location;
 
-        return ModuleDefinition.ReadModule(location).GetTypes().First(x => x.Name == typeof(T).Name);
+        return ModuleDefinition.ReadModule(location)
+            .GetTypes()
+            .First(x => x.Name == typeof(T).Name);
     }
 }
